be8ccd834eaf5b013ba83455c14cff5c542173cc
202110861 <dbsalsk2514@naver.com>
2025-10-15 13:09:21 +0900

    feat : 기능 1 구현(쉼표, 콜론을 구분자로 가지는 문자열의 숫자 합 반환)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import readLine from "readline";
+
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let sum = 0;
+
 class App {
-  async run() {}
+  async run() {
+    rl.question("덧셈할 문자열을 입력해 주세요.\n", (answer) => {
+      answer.split(/[,\n:]/).forEach((num) => {
+        sum += parseInt(num);
+      });
+      console.log("결과 : " + sum);
+      rl.close();
+    });
+  }
 }
 
 export default App;
