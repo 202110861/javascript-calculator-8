be8393d5a2cc7c9515f2460f6ccba0109dde0f92
202110861 <dbsalsk2514@naver.com>
2025-10-15 13:34:41 +0900

    feat : 커스텀 구분자 지정 시 커스텀 구분자로 문자열 구분

--- src/App.js
+++ src/App.js
@@ -10,9 +10,18 @@ let sum = 0;
 class App {
   async run() {
     rl.question("덧셈할 문자열을 입력해 주세요.\n", (answer) => {
-      answer.split(/[,\n:]/).forEach((num) => {
-        sum += parseInt(num);
-      });
+      if (answer.includes("," || ":")) {
+        answer.split(/[,\n:]/).forEach((num) => {
+          sum += parseInt(num);
+        });
+      } else if (answer.includes("//")) {
+        const CUSTOM_DELIMITER = answer.split("//")[1][0];
+        const SPLIT_ANSWER = answer.split("\\n")[1];
+
+        SPLIT_ANSWER.split(CUSTOM_DELIMITER).forEach((num) => {
+          sum += parseInt(num);
+        });
+      }
       console.log("결과 : " + sum);
       rl.close();
     });
